89fa0485be841e65c9a53af28f189752ba6a9dd6
Zor Abbanock <emirate1909@gmail.com>
2024-06-20 13:40:15 +0400

    cells game

--- main.js
+++ main.js
@@ -22,17 +22,53 @@
 //    resArea.textContent = Math.max(...resArray)
 //})
 
-const number = Math.floor(Math.random() * 100)
-console.log(number)
+//ИГРА УГАДАЙ ЧИСЛО
+//const number = Math.floor(Math.random() * 100)
+//
+//const userInput = document.querySelector('.user-text')
+//const btn = document.querySelector('.btn')
+//const outputNode = document.querySelector('.output')
+//
+//btn.addEventListener('click', () => {
+//    console.log(number)
+//    if (userInput.value === '') outputNode.textContent = 'Ты не ввел число'
+//    if (userInput.value > number) outputNode.textContent = 'Введи число поменьше'
+//    if (userInput.value < number) outputNode.textContent = 'Введи число побольше'
+//    if (userInput.value == number) outputNode.textContent = 'Верно!!!!'
+//})
 
-const userInput = document.querySelector('.user-text')
-const btn = document.querySelector('.btn')
-const outputNode = document.querySelector('.output')
+const wrapperBlock = document.getElementById("wrapperBlock");
 
-btn.addEventListener('click', () => {
-    console.log(number)
-    if (userInput.value === '') outputNode.textContent = 'Ты не ввел число'
-    if (userInput.value > number) outputNode.textContent = 'Введи число поменьше'
-    if (userInput.value < number) outputNode.textContent = 'Введи число побольше'
-    if (userInput.value == number) outputNode.textContent = 'Верно!!!!'
+function getRandCell() {
+    let cellsArr = []
+    for(let i = 0; i < 3; i++) {
+        cellsArr.push(Math.floor(Math.random() * 100))
+    }
+    return cellsArr.map(item => `number${item}`)
+}
+
+window.onload = function(){
+  for(let i=1; i<=100; i++){
+    let square = document.createElement("div");
+    square.classList.add("square");
+      square.classList.add(`number${i}`)
+    square.innerText =" ";
+    
+    wrapperBlock.appendChild(square);
+  }  
+}
+
+console.log(getRandCell().length)
+let cells = getRandCell()
+
+wrapperBlock.addEventListener('click', event => {
+    console.log(event.target.classList)
+    console.log(cells)
+    for(let i = 0; i < cells.length; i++ ) {
+        if (event.target.classList.contains(`${cells[i]}`)) {
+            event.target.classList.add('green')
+        } else event.target.classList.add('red')
+    }
 })
+
+
